018bb3722f3e5c4551a81b2903e60262f4680d59
Люция <lvaleeva87@gmail.com>
2021-11-15 16:28:15 +0500

    added model User, forming Serializer

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=64, unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
