a5d2487546727f66eb38e7a5a0832426413ea1b9
Люция <lvaleeva87@gmail.com>
2021-12-01 21:35:27 +0500

    added DjangoModelPermissionsOrAnonReadOnly

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("username", "first_name", "last_name", "email", "password")
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
