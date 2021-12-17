29ce6b40fbe6b07d0f89f4f4191617df7edec6b4
Люция <lvaleeva87@gmail.com>
2021-12-17 17:17:15 +0500

    GraphQL schema

--- todo/settings.py
+++ todo/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework.authtoken',
     'drf_yasg',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -156,3 +157,7 @@ REST_FRAMEWORK = {
 
 if DEBUG:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'].append('rest_framework.renderers.BrowsableAPIRenderer')
+
+GRAPHENE = {
+    "SCHEMA": "todo.schema.schema"
+}
