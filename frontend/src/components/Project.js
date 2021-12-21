f5dd0d5256bf7cac0ca700d0538243c84a4cdaae
Люция <lvaleeva87@gmail.com>
2021-12-21 15:26:14 +0500

    поиск по части названия проекта

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -13,20 +13,55 @@ const ProjectItem = ({project, users, deleteProject}) => {
    )
 }
 
-const ProjectList = ({projects, users, deleteProject}) => {
-   return (
-   <div>
-       <table>
-             <th>id</th>
-             <th>Name</th>
-             <th>User</th>
-             <th></th><th></th>
-            {projects.map((project) => <ProjectItem project={project} deleteProject={deleteProject}/>)}
-       </table>
-       <Link to={'/projects/create'}>Create</Link>
-   </div>
-   )
-}
+class ProjectList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {'filter': '', 'filtered_projects': props.projects}
+    }
 
+    handleChange(event)
+    {
+        this.setState(
+                {
+                    [event.target.name]: event.target.value
+                }
+            );
+        console.log(event.target.name, '=', event.target.value)
+    }
+
+    handleFilterChange(event) {
+        this.setState(
+                        {
+                            filter: event.target.value,
+                            filtered_projects: this.props.projects.filter((item) => item.name.toUpperCase().indexOf(event.target.value.toUpperCase()) != -1)
+                        }
+                    );
+        console.log(event.target.name, '=', event.target.value)
+    }
+
+    render() {
+        return (
+        <div>
+            <label>Фильтр</label>
+            <input type="text"  name="filter" value={this.state.filter} onChange={(event)=>this.handleFilterChange(event)} />
+            <table>
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>Name</th>
+                        <th>User</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.filtered_projects.map((project)=> <ProjectItem project={project} users={this.props.users} deleteProject={this.props.deleteProject} updateProject={this.props.updateProject}/>)}
+                </tbody>
+            </table>
+            <Link to={'/projects/create'}>Create</Link>
+        </div>
+        );
+    }
+}
 
 export default ProjectList;
